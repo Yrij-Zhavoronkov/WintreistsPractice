c3c499209880edc5685d2c0d0522832a8c30a797
Wintreist <smetaninvikenti@yandex.ru>
2023-08-08 15:27:36 +0300

    Подредачил

--- xgtf_to_excel/xgtf_to_excel.py
+++ xgtf_to_excel/xgtf_to_excel.py
@@ -3,6 +3,12 @@ import pandas as pd
 import xml.etree.ElementTree as ET
 from argparse import ArgumentParser
 
+
+
+def get_from_video(what:str):
+    return 1
+
+
 # Аргументы
 parser = ArgumentParser()
 parser.add_argument('--work-dir')
@@ -12,21 +18,32 @@ namespace = parser.parse_args()
 data = []
 for i in os.listdir(namespace.work_dir):
     # Условие для обработки .xgtf
-    if (i.find(".xgtf") == -1):
+    if i.find(".xgtf") == -1:
         continue
     # Подготовка
-    data.append([])
-    tree = ET.parse(namespace.work_dir+"\\"+i)
+    try:
+        tree = ET.parse(namespace.work_dir+"\\"+i)
+    except ET.ParseError:
+        data.append([i, 0, 0, 0, 0, None])
+        continue
+    print(namespace.work_dir+"\\"+i)
     root_data_sourcefile = tree.getroot().find('./{http://lamp.cfar.umd.edu/viper#}data/{http://lamp.cfar.umd.edu/viper#}sourcefile')
+    data.append([])
     # Имя
-    data[-1].append(root_data_sourcefile.attrib['filename'])
+    data[-1].append(i)
     # Количество объектов
     count_of_objects = len(root_data_sourcefile.findall('./{http://lamp.cfar.umd.edu/viper#}object'))
     data[-1].append(count_of_objects)
     # Длинна видео (секунд)
     root_data_sourcefile_file = root_data_sourcefile.find('./{http://lamp.cfar.umd.edu/viper#}file')
-    numframes = int(root_data_sourcefile_file.find('./{http://lamp.cfar.umd.edu/viper#}attribute[@name="NUMFRAMES"]/').attrib['value']) # NUMFRAMES
-    framerate = float(root_data_sourcefile_file.find('.//{http://lamp.cfar.umd.edu/viper#}attribute[@name="FRAMERATE"]/').attrib['value']) # FRAMERATE
+    try:
+        numframes = int(root_data_sourcefile_file.find('./{http://lamp.cfar.umd.edu/viper#}attribute[@name="NUMFRAMES"]/').attrib['value']) # NUMFRAMES
+    except AttributeError:
+        numframes = get_from_video("numframes")
+    try:
+        framerate = float(root_data_sourcefile_file.find('.//{http://lamp.cfar.umd.edu/viper#}attribute[@name="FRAMERATE"]/').attrib['value']) # FRAMERATE
+    except AttributeError:
+        framerate = get_from_video("framerate")
     # Расчет времени
     time = numframes / framerate
     hours = int(time // 3600)
@@ -42,8 +59,8 @@ for i in os.listdir(namespace.work_dir):
     data[-1].append(count_of_objects / numframes)
     # Классы
     classes = []
-    for i in root_data_sourcefile.findall('./{http://lamp.cfar.umd.edu/viper#}object/{http://lamp.cfar.umd.edu/viper#}attribute[@name="Class"]/{http://lamp.cfar.umd.edu/viperdata#}svalue'):
-        classes.append(i.attrib['value'])
+    for objects in root_data_sourcefile.findall('./{http://lamp.cfar.umd.edu/viper#}object/{http://lamp.cfar.umd.edu/viper#}attribute[@name="Class"]/{http://lamp.cfar.umd.edu/viperdata#}svalue'):
+        classes.append(objects.attrib['value'])
     data[-1].append(','.join(set(classes)))
 df = pd.DataFrame(data, columns=['Имя', 'Количество объектов', 'Длинна видео (секунд)', 'Длинна видео (кадры)', 'Среднее кол-во объектов на кадре', 'Классы'])
 df.to_excel(namespace.result_dir)
