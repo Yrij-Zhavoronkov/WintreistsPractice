97715f77c0a4b55b0194cfcd791e1bc572163294
Wintreist <smetaninvikenti@yandex.ru>
2023-08-22 15:50:32 +0300

    чет тыкнул

--- gui-for-tester-tracker/gui_tracker.py
+++ gui-for-tester-tracker/gui_tracker.py
@@ -6,8 +6,7 @@ from PyQt6.QtWidgets import (
     QApplication, 
     QMainWindow, 
     QFileDialog,
-    QHBoxLayout,
-    QVBoxLayout)
+    QListWidget,)
 from PyQt6 import uic, QtGui
 from PyQt6.QtCore import QSettings, Qt
 
@@ -45,7 +44,8 @@ class MainWindow(QMainWindow):
         self.toolButton_select_path_to_epf_file = self.findChild(QToolButton, 'toolButton_select_path_to_epf_file')
         self.toolButton_add_class = self.findChild(QToolButton, 'toolButton_add_class')
         
-        self.verticalLayout_list_classes = self.findChild(QVBoxLayout, 'verticalLayout_list_classes')
+        self.listWidget_list_classes = self.findChild(QListWidget, 'listWidget_list_classes')
+        
         
         # Параметры трекера
         self.checkBox_hide_stationary_objects = self.findChild(QCheckBox, 'checkBox_hide_stationary_objects')
@@ -78,30 +78,10 @@ class MainWindow(QMainWindow):
 
         self.pushButton_start_work.clicked.connect(self.startWork)
 
-        self.toolButton_add_class.clicked.connect(self.addClass)
-        for layout in self.verticalLayout_list_classes.findChildren(QHBoxLayout):
-            toolbutton = layout.itemAt(1).widget()
-            toolbutton.clicked.connect(partial(self.removeClass, layout))
         
         # ~~~~~~~~
 
-    def removeClass(self, layoutThisClass:QHBoxLayout):
-        for index in range(layoutThisClass.count()-1, -1,-1):
-            layoutThisClass.itemAt(index).widget().setParent(None)
-        layoutThisClass.setParent(None)
-        pass
 
-    def addClass(self):
-        if self.lineEdit_add_new_class.text():
-            horizontalLayout = QHBoxLayout()
-            checkbox = QCheckBox(text=self.lineEdit_add_new_class.text())
-            self.lineEdit_add_new_class.setText("")
-            toolbutton = QToolButton(text="-")
-            toolbutton.clicked.connect(partial(self.removeClass, horizontalLayout))
-            horizontalLayout.addWidget(checkbox)
-            horizontalLayout.addWidget(toolbutton)
-            self.verticalLayout_list_classes.addLayout(horizontalLayout)
-        pass
 
     def openFileExplorer(self, input_directory:QLineEdit, text:str=None):
         text = text if text else "Выберите директорию"
@@ -120,10 +100,7 @@ class MainWindow(QMainWindow):
 
     def startWork(self):
         testing_classes = []
-        for index in range(self.verticalLayout_list_classes.count()):
-            layout = self.verticalLayout_list_classes.itemAt(index)
-            if layout.itemAt(0).widget().checkState() == Qt.CheckState.Checked:
-                testing_classes.append(layout.itemAt(0).widget().text())
+
         self.hide()
         # sub.run([sys.executable, 'tester.py'])
         import time
@@ -147,22 +124,6 @@ class MainWindow(QMainWindow):
         self.checkBox_use_MOT.setChecked(bool(self.settings.value(self.checkBox_use_MOT.objectName(), False)))
 
 
-        classes = self.settings.value(self.verticalLayout_list_classes.objectName(), {})
-        if classes:
-            layout = self.verticalLayout_list_classes.itemAt(0)
-            for index in range(layout.count()-1, -1,-1):
-                layout.itemAt(index).widget().setParent(None)
-            layout.setParent(None)
-        
-        for class_name, check_state in classes.items():
-            horizontalLayout = QHBoxLayout()
-            checkbox = QCheckBox(text=class_name)
-            checkbox.setChecked(check_state)
-            toolbutton = QToolButton(text="-")
-            horizontalLayout.addWidget(checkbox)
-            horizontalLayout.addWidget(toolbutton)
-            self.verticalLayout_list_classes.addLayout(horizontalLayout)
-        pass
 
     def saveState(self):
         self.settings.setValue(self.lineEdit_confidence_threshold.objectName(), self.lineEdit_confidence_threshold.text())
@@ -179,12 +140,6 @@ class MainWindow(QMainWindow):
         self.settings.setValue(self.checkBox_hide_stationary_objects.objectName(), True if self.checkBox_hide_stationary_objects.checkState() == Qt.CheckState.Checked else '')
         self.settings.setValue(self.checkBox_use_MOT.objectName(), True if self.checkBox_use_MOT.checkState() == Qt.CheckState.Checked else '')
 
-        classes = {}
-        for layout in self.verticalLayout_list_classes.findChildren(QHBoxLayout):
-            widget = layout.itemAt(0).widget()
-            classes[widget.text()] = widget.checkState() == Qt.CheckState.Checked
-        self.settings.setValue(self.verticalLayout_list_classes.objectName(), classes)
-        pass
 
 
     def closeEvent(self, event):
