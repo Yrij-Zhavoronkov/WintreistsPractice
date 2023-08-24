6be7ac22619851e849fabee58a17908e889ff097
Wintreist <smetaninvikenti@yandex.ru>
2023-08-24 17:27:54 +0300

    codeStyle?

--- gui-for-tester-tracker/gui_tracker.py
+++ gui-for-tester-tracker/gui_tracker.py
@@ -1,81 +1,84 @@
+import sys
+import os
+from functools import partial
+from typing import Callable
 from PyQt6.QtWidgets import (
-    QPushButton, 
     QLineEdit, 
     QToolButton,
     QCheckBox, 
     QApplication, 
     QMainWindow, 
     QFileDialog,
-    QListWidget,
     QHBoxLayout,
     QWidget,
     QListWidgetItem,
     QSpacerItem,
     QSizePolicy,)
-from PyQt6 import uic, QtGui
-from PyQt6.QtCore import QSettings, Qt, QSize, pyqtSignal
-import sys
-import os
-from functools import partial
-import sys
-from typing import Callable
+from PyQt6 import QtGui
+from PyQt6.QtCore import QSettings, Qt, QSize
 from tester import main as testerTrackerMain
 from gui import Ui_MainWindow
 
 class MainWindow(QMainWindow, Ui_MainWindow):
     # Сигналы
-    onProgressWork = pyqtSignal(int, int, str, str)
+    # onProgressWork = pyqtSignal(int, int, str, str)
     # ~~~~~~~
     def __init__(self):
         super(MainWindow, self).__init__()
         self.setupUi(self)
-
-        
+        self.add_class(lambda: "Human", True, False)
         # Меню
         # Конфиг
-        self.action_save_config.triggered.connect(self.saveState)
-        self.action_load_config.triggered.connect(self.loadState)
+        self.action_save_config.triggered.connect(self.save_state)
+        self.action_load_config.triggered.connect(self.load_state)
         # ~~~~~
         # Сигналы
-        self.toolButton_select_markup_dir.clicked.connect(partial(self.openFileExplorer, self.lineEdit_to_markup_dir, "Выберите путь к папке с разметкой"))
-        self.toolButton_select_networks_dir.clicked.connect(partial(self.openFileExplorer, self.lineEdit_to_networks_dir, "Выберите путь к папке с сетями"))
-        self.toolButton_select_result_dir.clicked.connect(partial(self.openFileExplorer, self.lineEdit_to_result_dir, "Выберите путь к папке для сохранения результатов"))
-        self.toolButton_select_path_to_siamese_neural_network.clicked.connect(partial(self.openFileLocation, self.lineEdit_path_to_siamese_neural_network, "Выберите путь к сиамской нейронной сети"))
-        self.toolButton_select_path_to_epf_file.clicked.connect(partial(self.openFileLocation, self.lineEdit_path_to_epf_file, "Выберите путь к .epf файлу", "*.epf"))
-        self.toolButton_add_class.clicked.connect(partial(self.addClass, self.lineEdit_add_new_class.text, True, True))
+        self.toolButton_select_markup_dir.clicked.connect(
+            partial(self.open_file_explorer, self.lineEdit_to_markup_dir, "Выберите путь к папке с разметкой"))
+        self.toolButton_select_networks_dir.clicked.connect(
+            partial(self.open_file_explorer, self.lineEdit_to_networks_dir, "Выберите путь к папке с сетями"))
+        self.toolButton_select_result_dir.clicked.connect(
+            partial(self.open_file_explorer, self.lineEdit_to_result_dir, "Выберите путь к папке для сохранения результатов"))
+        self.toolButton_select_path_to_siamese_neural_network.clicked.connect(
+            partial(self.open_file_location, self.lineEdit_path_to_siamese_neural_network, "Выберите путь к сиамской нейронной сети"))
+        self.toolButton_select_path_to_epf_file.clicked.connect(
+            partial(self.open_file_location, self.lineEdit_path_to_epf_file, "Выберите путь к .epf файлу", "*.epf"))
+        self.toolButton_add_class.clicked.connect(
+            partial(self.add_class, self.lineEdit_add_new_class.text, True, True))
 
-        self.lineEdit_add_new_class.returnPressed.connect(partial(self.addClass, self.lineEdit_add_new_class.text, True, True))
-        self.lineEdit_to_networks_dir.textChanged.connect(self.allNeededLineEditsAreNotEmpty)
-        self.lineEdit_to_markup_dir.textChanged.connect(self.allNeededLineEditsAreNotEmpty)
+        self.lineEdit_add_new_class.returnPressed.connect(
+            partial(self.add_class, self.lineEdit_add_new_class.text, True, True))
+        self.lineEdit_to_networks_dir.textChanged.connect(self.all_needed_line_edits_are_not_empty)
+        self.lineEdit_to_markup_dir.textChanged.connect(self.all_needed_line_edits_are_not_empty)
 
-        self.pushButton_start_work.clicked.connect(self.startWork)
-        pass
-    def allNeededLineEditsAreNotEmpty(self):
+        self.pushButton_start_work.clicked.connect(self.start_work)
+        
+    def all_needed_line_edits_are_not_empty(self):
         allNeededLineEdits = [
             self.lineEdit_to_networks_dir,
             self.lineEdit_to_markup_dir,
         ]
         is_filled = all(lineEdit.text() for lineEdit in allNeededLineEdits)
         self.pushButton_start_work.setEnabled(is_filled)
-        pass
-    def openFileExplorer(self, input_directory:QLineEdit, text:str=None):
+
+    def open_file_explorer(self, input_directory:QLineEdit, text:str=None):
         text = text if text else "Выберите директорию"
         directory = QFileDialog.getExistingDirectory(self, text)
         if directory:
             input_directory.setText(directory)
-        pass
-    def openFileLocation(self, lineEdit_file_location:QLineEdit, text:str=None, filter:str=None):
+
+    def open_file_location(self, lineEdit_file_location:QLineEdit, text:str=None, filter:str=None):
         text = text if text else "Выберите файл"
         filter = filter if filter else None
         file_location = QFileDialog.getOpenFileName(self, text, filter=filter)
         if file_location[0]:
             lineEdit_file_location.setText(file_location[0])
-        pass
-    def startWork(self):
+
+    def start_work(self):
         testing_classes = []
         for index in range(self.listWidget_list_classes.count()):
             item = self.listWidget_list_classes.item(index)
-            widget = self.listWidget_list_classes.itemWidget(item)
+            widget:ListItemClass = self.listWidget_list_classes.itemWidget(item)
             if widget.checkbox.isChecked():
                 testing_classes.append(widget.checkbox.text())
         self.hide()
@@ -93,63 +96,84 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             minDetectionTriggers=int(self.lineEdit_minimum_number_of_triggers.text()),
             confThreshold=float(self.lineEdit_confidence_threshold.text()),
             siameseFile=self.lineEdit_path_to_siamese_neural_network.text(),
-            onProgressCallback=self.onProgressWork.emit,
+            # onProgressCallback=self.onProgressWork.emit,
         )
         self.show()
-        pass
-    def loadState(self):
+
+    def load_state(self):
         path_to_settings = QFileDialog.getOpenFileName(self, 'Выберите файл настроек')
         if path_to_settings[0]:
             self.settings = QSettings(path_to_settings[0], QSettings.Format.IniFormat)
 
-            self.lineEdit_to_markup_dir.setText(self.settings.value(self.lineEdit_to_markup_dir.objectName(), ""))
-            self.lineEdit_to_networks_dir.setText(self.settings.value(self.lineEdit_to_networks_dir.objectName(), ""))
-            self.lineEdit_to_result_dir.setText(self.settings.value(self.lineEdit_to_result_dir.objectName(), "/results"))
-            self.lineEdit_path_to_siamese_neural_network.setText(self.settings.value(self.lineEdit_path_to_siamese_neural_network.objectName(), ""))
-            self.lineEdit_path_to_epf_file.setText(self.settings.value(self.lineEdit_path_to_epf_file.objectName(), ""))
-            self.lineEdit_confidence_threshold.setText(self.settings.value(self.lineEdit_confidence_threshold.objectName(), "0.3"))
-            self.lineEdit_minimum_number_of_triggers.setText(self.settings.value(self.lineEdit_minimum_number_of_triggers.objectName(), "6"))
-            self.lineEdit_IOU_threshold.setText(self.settings.value(self.lineEdit_IOU_threshold.objectName(), "0.3"))
-            self.lineEdit_sensitivity_detector_stationary_objects.setText(self.settings.value(self.lineEdit_sensitivity_detector_stationary_objects.objectName(), "0.5"))
-            self.lineEdit_fps.setText(self.settings.value(self.lineEdit_fps.objectName(), "13"))
+            self.lineEdit_to_markup_dir.setText(
+                self.settings.value(self.lineEdit_to_markup_dir.objectName(), ""))
+            self.lineEdit_to_networks_dir.setText(
+                self.settings.value(self.lineEdit_to_networks_dir.objectName(), ""))
+            self.lineEdit_to_result_dir.setText(
+                self.settings.value(self.lineEdit_to_result_dir.objectName(), "/results"))
+            self.lineEdit_path_to_siamese_neural_network.setText(
+                self.settings.value(self.lineEdit_path_to_siamese_neural_network.objectName(), ""))
+            self.lineEdit_path_to_epf_file.setText(
+                self.settings.value(self.lineEdit_path_to_epf_file.objectName(), ""))
+            self.lineEdit_confidence_threshold.setText(
+                self.settings.value(self.lineEdit_confidence_threshold.objectName(), "0.3"))
+            self.lineEdit_minimum_number_of_triggers.setText(
+                self.settings.value(self.lineEdit_minimum_number_of_triggers.objectName(), "6"))
+            self.lineEdit_IOU_threshold.setText(
+                self.settings.value(self.lineEdit_IOU_threshold.objectName(), "0.3"))
+            self.lineEdit_sensitivity_detector_stationary_objects.setText(
+                self.settings.value(self.lineEdit_sensitivity_detector_stationary_objects.objectName(), "0.5"))
+            self.lineEdit_fps.setText(
+                self.settings.value(self.lineEdit_fps.objectName(), "13"))
 
-            self.checkBox_hide_stationary_objects.setChecked(bool(self.settings.value(self.checkBox_hide_stationary_objects.objectName(), False)))
-            self.checkBox_use_MOT.setChecked(bool(self.settings.value(self.checkBox_use_MOT.objectName(), False)))
+            self.checkBox_hide_stationary_objects.setChecked(
+                bool(self.settings.value(self.checkBox_hide_stationary_objects.objectName(), False)))
+            self.checkBox_use_MOT.setChecked(
+                bool(self.settings.value(self.checkBox_use_MOT.objectName(), False)))
 
             self.listWidget_list_classes.clear()
             items = self.settings.value(self.listWidget_list_classes.objectName(), {
                 "Human":True
             })
             for class_name, check_state in items.items():
-                self.addClass(lambda: class_name, check_state)
-        pass
-    def saveState(self):
+                self.add_class(lambda: class_name, check_state)
+
+    def save_state(self):
         path_to_settings = QFileDialog.getSaveFileName(self, "Выберите путь\файл настроек")
         if path_to_settings[0]:
             self.settings = QSettings(path_to_settings[0], QSettings.Format.IniFormat)
-
-            self.settings.setValue(self.lineEdit_confidence_threshold.objectName(), self.lineEdit_confidence_threshold.text())
-            self.settings.setValue(self.lineEdit_minimum_number_of_triggers.objectName(), self.lineEdit_minimum_number_of_triggers.text())
-            self.settings.setValue(self.lineEdit_IOU_threshold.objectName(), self.lineEdit_IOU_threshold.text())
-            self.settings.setValue(self.lineEdit_sensitivity_detector_stationary_objects.objectName(), self.lineEdit_sensitivity_detector_stationary_objects.text())
-            self.settings.setValue(self.lineEdit_fps.objectName(), self.lineEdit_fps.text())
-            self.settings.setValue(self.lineEdit_path_to_siamese_neural_network.objectName(), self.lineEdit_path_to_siamese_neural_network.text())
-            self.settings.setValue(self.lineEdit_path_to_epf_file.objectName(), self.lineEdit_path_to_epf_file.text())
-            self.settings.setValue(self.lineEdit_to_markup_dir.objectName(), self.lineEdit_to_markup_dir.text())
-            self.settings.setValue(self.lineEdit_to_networks_dir.objectName(), self.lineEdit_to_networks_dir.text())
-            self.settings.setValue(self.lineEdit_to_result_dir.objectName(), self.lineEdit_to_result_dir.text())
-
-            self.settings.setValue(self.checkBox_hide_stationary_objects.objectName(), True if self.checkBox_hide_stationary_objects.checkState() == Qt.CheckState.Checked else '')
-            self.settings.setValue(self.checkBox_use_MOT.objectName(), True if self.checkBox_use_MOT.checkState() == Qt.CheckState.Checked else '')
-
+            self.settings.setValue(self.lineEdit_confidence_threshold.objectName(), 
+                                   self.lineEdit_confidence_threshold.text())
+            self.settings.setValue(self.lineEdit_minimum_number_of_triggers.objectName(), 
+                                   self.lineEdit_minimum_number_of_triggers.text())
+            self.settings.setValue(self.lineEdit_IOU_threshold.objectName(), 
+                                   self.lineEdit_IOU_threshold.text())
+            self.settings.setValue(self.lineEdit_sensitivity_detector_stationary_objects.objectName(), 
+                                   self.lineEdit_sensitivity_detector_stationary_objects.text())
+            self.settings.setValue(self.lineEdit_fps.objectName(), 
+                                   self.lineEdit_fps.text())
+            self.settings.setValue(self.lineEdit_path_to_siamese_neural_network.objectName(), 
+                                   self.lineEdit_path_to_siamese_neural_network.text())
+            self.settings.setValue(self.lineEdit_path_to_epf_file.objectName(), 
+                                   self.lineEdit_path_to_epf_file.text())
+            self.settings.setValue(self.lineEdit_to_markup_dir.objectName(), 
+                                   self.lineEdit_to_markup_dir.text())
+            self.settings.setValue(self.lineEdit_to_networks_dir.objectName(), 
+                                   self.lineEdit_to_networks_dir.text())
+            self.settings.setValue(self.lineEdit_to_result_dir.objectName(), 
+                                   self.lineEdit_to_result_dir.text())
+            self.settings.setValue(self.checkBox_hide_stationary_objects.objectName(), 
+                                   True if self.checkBox_hide_stationary_objects.checkState() == Qt.CheckState.Checked else '')
+            self.settings.setValue(self.checkBox_use_MOT.objectName(), 
+                                   True if self.checkBox_use_MOT.checkState() == Qt.CheckState.Checked else '')
             classes = {}
             for index in range(self.listWidget_list_classes.count()):
                 item = self.listWidget_list_classes.item(index)
                 widget:ListItemClass = self.listWidget_list_classes.itemWidget(item)
                 classes[widget.checkbox.text()] = widget.checkbox.isChecked()
             self.settings.setValue(self.listWidget_list_classes.objectName(), classes)
-        pass
-    def addClass(self, class_name:Callable[[], str], check_state, isAddedFromLineEdit=False):
+
+    def add_class(self, class_name:Callable[[], str], check_state, isAddedFromLineEdit=False):
         if class_name():
             widget = ListItemClass(class_name(), check_state)
             list_item = QListWidgetItem()
@@ -157,14 +181,12 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             list_item.setSizeHint(QSize(0, 40))
             self.listWidget_list_classes.addItem(list_item)
             self.listWidget_list_classes.setItemWidget(list_item, widget)
-            widget.toolButton.clicked.connect(partial(self.removeClass, list_item))
+            widget.toolButton.clicked.connect(partial(self.remove_class, list_item))
             if isAddedFromLineEdit:
                 self.lineEdit_add_new_class.setText("")
-        pass
-    def removeClass(self, item):
+
+    def remove_class(self, item):
         self.listWidget_list_classes.takeItem(self.listWidget_list_classes.row(item))
-        pass
-    pass
 
 class ListItemClass(QWidget):
     def __init__(self, class_name, check_state):
@@ -178,13 +200,11 @@ class ListItemClass(QWidget):
         self.layout.addWidget(self.checkbox)
         self.layout.addItem(self.spacer)
         self.layout.addWidget(self.toolButton)
-        pass
-    pass
 
-path = os.path.dirname(os.path.realpath(__file__))
+_path = os.path.dirname(os.path.realpath(__file__))
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    app.setWindowIcon(QtGui.QIcon(os.path.join(path, 'icon.png')))
+    app.setWindowIcon(QtGui.QIcon(os.path.join(_path, 'icon.png')))
     window = MainWindow()
     window.setWindowTitle("GUI for tester tracker")
     window.show()
